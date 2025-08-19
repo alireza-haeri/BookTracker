cdb67ad10ffadbae3ef438236167d32cf5a624b0
alireza-haeri <alireza.haeri.dev@gmail.com>
2025-08-19 17:15:02 +0330

    change Program.cs

--- src/BookTracker/Program.cs
+++ src/BookTracker/Program.cs
@@ -1,6 +1,22 @@
+using BookTracker.MongoDb.Extensions;
+using Carter;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder
+    .ConfigureMongoDb()
+    .ConfigureMongoDbEntities();
+
+builder.Services.AddCarter();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapOpenApi();
+app.MapScalarApiReference();
+
+await app.UseMongoDbEntitiesAsync();
+
+app.MapCarter();
 
 app.Run();

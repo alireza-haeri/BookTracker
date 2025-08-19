b5620cb5aa64fd95c834a65ef2b12590494e2185
alireza-haeri <alireza.haeri.dev@gmail.com>
2025-08-19 16:58:45 +0330

    Create Entity Models

---
+++ src/BookTracker/EntityModels/Book.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BookTracker.EntityModels;
+
+public class Book
+{
+    [NotMapped] [BsonIgnore] public const string CollectionName = "Book";
+
+    public ObjectId Id { get; set; }
+    public ObjectId UserId { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+
+    public string Notes { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdateAt { get; set; }
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public BookStatus Status { get; set; }
+}
+
+public enum BookStatus
+{
+    /// <summary>
+    /// The user is currently reading this book.
+    /// </summary>
+    Reading = 0,
+
+    /// <summary>
+    /// The user has finished reading this book.
+    /// </summary>
+    Completed = 1,
+
+    /// <summary>
+    /// The user wants to read this book in the future.
+    /// </summary>
+    Wishlist = 2,
+
+    /// <summary>
+    /// The user has paused reading this book temporarily.
+    /// </summary>
+    OnHold = 3,
+
+    /// <summary>
+    /// The user started reading but decided to abandon the book.
+    /// </summary>
+    Abandoned = 4,
+
+    /// <summary>
+    /// The user is reading this book again.
+    /// </summary>
+    ReReading = 5
+}
---
+++ src/BookTracker/EntityModels/User.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+
+namespace BookTracker.EntityModels;
+
+public class User
+{
+    public ObjectId Id { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Password { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdateAt { get; set; }
+
+    public List<Book> Books { get; set; }
+}
